353102586b194bbd8e4f4552b2b347e604168f44
Simon Jackson (Netbook) <jackokring@gmail.com>
2014-12-15 17:16:56 +0000

    post vars

--- main.js
+++ main.js
@@ -11,7 +11,14 @@ app.all('/echo/:echo',
 	}
 );
 
-//php-cgi .deb processing with $_GET too, no POST vars or cookies
+//POST and JSON styled POST input?
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+  extended: true
+})); 
+
+//php-cgi .deb processing with $_GET too, and also no file POST vars but not cookies
 //-q option surpresses headers from php-cgi
 var exec = require("child_process").exec;
 var qs = require('querystring');
@@ -19,6 +26,7 @@ var qs = require('querystring');
 app.all('/php/:script',
 	function(req, res, next) {
 		var query = qs.stringify(req.query , ' ');
+		var query += ' ' + qs.stringify(req.body , ' ');
 		exec("php-cgi -q " + req.param('script') + ".php " + query,
 			function (error, stdout, stderr) {
 				res.send(stdout);
